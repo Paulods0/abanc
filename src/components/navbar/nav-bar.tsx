c0d7422b662f1357dbd883243de9630b73809937
Paulods0 <pauloluguenda0@gmail.com>
2024-05-20 18:18:01 +0100

    fix:header

--- src/components/navbar/nav-bar.tsx
+++ src/components/navbar/nav-bar.tsx
@@ -18,22 +18,22 @@ const Navbar = () => {
   return (
     <header className="flex sticky top-0 flex-col pb-4 pt-1 border-b z-20 bg-white">
       <Container>
-        <section className="flex px-3 lg:px-0 items-center md:items-end justify-between">
+        <section className="flex px-3 lg:px-0 items-start md:items-end justify-between">
           <Link to={"/"}>
             <img
               src="/logo/logo-2.png"
-              className="w-20 h-auto md:w-36 object-cover"
+              className="w-24 h-auto md:w-36 object-contain"
               alt="logotipo"
             />
           </Link>
 
-          <nav className=" hidden md:hidden lg:flex items-center ">
+          <nav className=" hidden md:hidden lg:flex items-center pb-1">
             <ul className="flex items-center gap-x-6 ">
               {NAV_LINKS.map((link, i) => (
                 <motion.li key={i} className="relative cursor-pointer group">
                   <Link
                     to={`${link.path}/${link.links[0].link}`}
-                    className={`capitalize  text-sm hover:text-vermelho font-semibold duration-200 transition-all ${
+                    className={`capitalize text-sm hover:text-vermelho font-semibold duration-200 transition-all ${
                       currentPath === link.path ? "text-vermelho" : "text-black"
                     }`}
                   >
@@ -44,10 +44,11 @@ const Navbar = () => {
                 </motion.li>
               ))}
             </ul>
-            <SearchBar />
           </nav>
 
-          <div className="mt-2">
+          <SearchBar />
+
+          <div className="mt-2 flex lg:hidden">
             <MobileMenu />
           </div>
         </section>
