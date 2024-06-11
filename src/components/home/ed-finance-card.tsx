1fd0b117b184e785c03089d1a435ee0f6c56923a
Paulods0 <pauloluguenda0@gmail.com>
2024-06-11 14:46:23 +0100

    update:title background color

--- src/components/home/ed-finance-card.tsx
+++ src/components/home/ed-finance-card.tsx
@@ -10,29 +10,29 @@ const EdFinanceCard = ({ title }: Props) => {
     <CardScaleDownEffect>
       <Card className="bg-white rounded-3xl shadow-lg w-full p-4 flex flex-col">
         <CardHeader>
-          <CardTitle className="text-white text-lg px-4 py-1 font-semibold uppercase bg-black/60 w-full rounded-full">
+          <CardTitle className="text-white text-lg px-4 py-1 font-semibold uppercase bg-vermelho w-full rounded-full">
             {title}
           </CardTitle>
         </CardHeader>
 
         <CardContent>
-          <ul className="space-y-2 capitalize text-base text-zinc-500">
-            <li className=" flex justify-between items-center w-full px-4 py-1 bg-zinc-200 rounded-full">
+          <ul className="space-y-2 capitalize text-base text-white">
+            <li className=" flex justify-between items-center w-full px-4 py-1 bg-black/70 rounded-full">
               <span className="font-medium">maturidade</span>
               <span className="font-semibold">taxa</span>
             </li>
 
-            <li className="flex justify-between items-center w-full px-4 py-1 bg-zinc-200 rounded-full">
+            <li className="flex justify-between items-center w-full px-4 py-1 bg-black/20 rounded-full">
               <span className="font-medium">overnight</span>
               <span className="font-semibold">21.54%</span>
             </li>
 
-            <li className="flex justify-between items-center w-full px-4 py-1 bg-zinc-200 rounded-full">
+            <li className="flex justify-between items-center w-full px-4 py-1 bg-black/20 rounded-full">
               <span className="font-medium">1 mês</span>
               <span className="font-semibold">8.89%</span>
             </li>
 
-            <li className="flex justify-between items-center w-full px-4 py-1 bg-zinc-200 rounded-full">
+            <li className="flex justify-between items-center w-full px-4 py-1 bg-black/20 rounded-full">
               <span className="font-medium">3 meses</span>
               <span className="font-semibold">10.97%</span>
             </li>
